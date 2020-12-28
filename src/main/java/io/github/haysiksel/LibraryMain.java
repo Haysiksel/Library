7ecd45beeea410a3fd8e416c05fd1b7f8c7d1c07
cemicyz <cemicyz@pm.me>
2020-12-28 15:01:02 +0300

    🌌 first commit

---
+++ src/main/java/io/github/haysiksel/LibraryMain.java
@@ -0,0 +1,16 @@
+package io.github.haysiksel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LibraryMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
